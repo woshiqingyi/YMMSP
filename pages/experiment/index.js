ebe3ea4bd68600be95b050ac8eb7066a7dc77795
woshiqingyi <1355224991@qq.com>
2019-04-30 17:37:49 +0800

    1

---
+++ pages/experiment/index.js
@@ -0,0 +1,21 @@
+
+Page({
+  data: {
+
+  },
+
+  onLoad: function (options) {
+
+  },
+
+  onPullDownRefresh: function () {
+    console.log('11')
+  },
+
+  
+  onReachBottom: function () {
+    console.log('22')
+  },
+
+ 
+})
